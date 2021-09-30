ba7f8a10b878adbb5d3814f893456c30743404bf
steven96SaMo <60241435+steven96SaMo@users.noreply.github.com>
2021-09-29 22:02:06 -0500

    view added

--- src/app/view/view-employee/view-employee.component.ts
+++ src/app/view/view-employee/view-employee.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/model/employee';
+import { EmployeeService } from 'src/app/service/employee.service';
 
 @Component({
   selector: 'app-view-employee',
@@ -8,12 +11,80 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ViewEmployeeComponent implements OnInit {
 
+  employee: Employee;
+  employeeId = "";
+  isChecked = false;
+  valueJobTitle = "";
+  valueArea = "";
+  countries = [{ value: "", text: "" }];
+  name = "";
+  dateOfBirth = "";
+  country = "";
+  userName = "";
+  hiringDate = "";
+  state = false;
+  area = "";
+  jobTitle = "";
+  commission = "";
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private employeeService: EmployeeService,
   ) { }
 
   ngOnInit(): void {
+    this.loadCountries()
+    this.loadEmployee()
+  }
+
+  back() {
+    this.router.navigate(["/"])
+  }
+
+  loadCountries() {
+    this.employeeService.getCountries().subscribe(
+      p => {
+        this.countries = p !== undefined ? p : []
+      },
+      e => { console.log(e) },
+      () => {
+        console.log(this.countries)
+      }
+    )
+  }
+
+  loadEmployee() {
+    this.employeeId = this.route.snapshot.paramMap.get("id")!;
+    this.employeeService.getEmployee(this.employeeId).subscribe(
+      p => {
+        console.log(p);
+        this.employee = p.results[0] !== undefined ? p.results[0] : []
+      },
+      e => { console.log(e) },
+      () => {
+        this.loadData()
+      }
+    )
+  }
+
+  loadData() {
+    this.name = this.employee.name;
+    this.dateOfBirth = this.employee.dateOfBirth;
+    this.country = this.employee.country;
+    this.userName = this.employee.userName;
+    this.hiringDate = this.employee.hiringDate;
+    if (this.employee.state == "1") {
+      this.state = true;
+      this.isChecked = true
+    } else {
+      this.state = false;
+      this.isChecked = false;
+    }
+    this.valueArea = this.employee.area;
+    this.valueJobTitle = this.employee.jobTitle;
+    this.commission = this.employee.commission;
   }
 
 }
